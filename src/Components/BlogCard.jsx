76726d817d242f5f319ddd27304fd17a6cfea5c7
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-12-21 14:30:13 +0600

    cut off some text on the home page

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const BlogCard = ({blog}) => {
-    // let content =blog.Descri.content;
+    let content =blog.Descri.content;
     return (
         <div className="card bg-base-100 shadow-xl">
             <figure className="px-5 pt-5">
@@ -10,7 +10,7 @@ const BlogCard = ({blog}) => {
             </figure>
             <div className="card-body items-center">
                 <h2 className="card-title text-center">{blog.Title}</h2>
-                {/* <p>{Object.values(content)[0]}</p> */}
+                <p className='cut-off-text '>{Object.values(content)[0]}</p>
                 <div className="card-actions">
                     <button className="btn btn-primary"><Link to={`/home/${blog._id}`}>Read More</Link></button>
                 </div>
